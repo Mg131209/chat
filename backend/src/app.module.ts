ccc1e23e9347ac343ec792466f8ad04aecb39630
Mga131209 <miogalli@icloud.com>
2026-02-01 19:24:32 +0100

    switched to mariadb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,7 +9,7 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'mariadb',
       host: 'localhost',
       port: 3306,
       username: 'root',
